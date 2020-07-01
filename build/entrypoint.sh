1656d284eda7654e4b72a72aa93cade50526e66c
doogledor <a3dddog@gmail.com>
2020-07-01 15:04:18 -0700

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,9 +8,8 @@ set -e
 : ${INPUT_TAG:=$GITHUB_SHA}
 : ${INPUT_LATEST:=true}
 
-cd apps/dashboard
 
-docker build $INPUT_ARGS -t $INPUT_IMAGE:$INPUT_TAG .
+docker build $INPUT_ARGS -t $INPUT_IMAGE:$INPUT_TAG apps/dashboard
 docker tag $INPUT_IMAGE:$INPUT_TAG $INPUT_REGISTRY/$INPUT_IMAGE:$INPUT_TAG
 
 if [ $INPUT_LATEST = true ]; then
